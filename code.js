85a9ed84a8fa6c74c321eb9e3eb7a7a00ee1a0a5
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-03-16 14:00:34 -0400

    'commit it'

--- code.js
+++ code.js
@@ -3,6 +3,11 @@ function helloWorld() {
     return "Hello, World!";
 }
 function sayHello(name) {
+    if (name === undefined || name === null)
+        return "Hello, World!"
+    else if (name === true || name === false)
+        return "Hello, World!"
+    else
     return ("Hello, " + name + "!")
 }
 
